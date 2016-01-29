077fad1fc8528318929c491c6dbfc9af31b76f02
Joey <jlai@joeys-macbook-pro.local>
2016-01-29 07:47:25 -0700

    Initial Commit

---
+++ SwiftPredicates/SwiftPredicates.h
@@ -0,0 +1,19 @@
+//
+//  SwiftPredicates.h
+//  SwiftPredicates
+//
+//  Created by Joey Lai on 2016-01-29.
+//  Copyright © 2016 Joey Lai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftPredicates.
+FOUNDATION_EXPORT double SwiftPredicatesVersionNumber;
+
+//! Project version string for SwiftPredicates.
+FOUNDATION_EXPORT const unsigned char SwiftPredicatesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftPredicates/PublicHeader.h>
+
+
