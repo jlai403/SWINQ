84b0e290d0000892f0d2114432b548b6cb1a1083
Joey <jlai@Joeys-MacBook-Pro.local>
2016-02-04 21:48:42 -0700

    Refactored Array Extensions
    - split Array extensions based on Element class type

--- SwiftPredicates/ArrayExtensions.swift
+++ SwiftPredicates/ArrayExtensions.swift
@@ -5,13 +5,14 @@ extension Array {
     public func lastIndex() -> Int {
         return self.count - 1
     }
+}
+
+extension Array where Element: Equatable {
     
-    mutating func remove <T: Equatable> (object: T) {
+    mutating func remove(object: Element) {
         for index in lastIndex().stride(through: 0, by: -1) {
-            if let element = self[index] as? T {
-                if element == object {
-                    self.removeAtIndex(index)
-                }
+            if self[index] == object {
+                self.removeAtIndex(index)
             }
         }
     }
