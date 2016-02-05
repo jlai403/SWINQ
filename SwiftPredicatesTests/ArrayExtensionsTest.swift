8a22e0ba51990dd9ac55fb581c53588726a12866
Joey <jlai@Joeys-MacBook-Pro.local>
2016-02-05 11:03:32 -0700

    Fixed test build after renaming project

--- SwiftPredicatesTests/ArrayExtensionsTest.swift
+++ SwiftPredicatesTests/ArrayExtensionsTest.swift
@@ -1,5 +1,5 @@
 import XCTest
-@testable import SwiftPredicates
+@testable import SWINQ
 
 class ArrayExtensionsTests: XCTestCase {
     
--- SwiftPredicatesTests/ArrayPredicatesTests.swift
+++ SwiftPredicatesTests/ArrayPredicatesTests.swift
@@ -1,6 +1,6 @@
 
 import XCTest
-@testable import SwiftPredicates
+@testable import SWINQ
 
 class ArrayPredicatesTests: XCTestCase {
     
