94f6947d01d2a7c3e87d4937cb13cbcfbf0e27e2
Joey <j.lai@live.ca>
2016-03-16 23:15:36 -0600

    Rebuilt SWINQTest Target

---
+++ SWINQ/SWINQ-Swift.h
@@ -0,0 +1,14 @@
+//
+//  SWINQ-swift.h
+//  SWINQ
+//
+//  Created by Joey Lai on 2016-03-16.
+//  Copyright © 2016 Joey Lai. All rights reserved.
+//
+
+#ifndef SWINQ_swift_h
+#define SWINQ_swift_h
+
+#import "SWINQ/SWINQ-Swift"
+
+#endif /* SWINQ_swift_h */
